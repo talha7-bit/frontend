702de4c649aeaca99744726549c02463a96187fb
talha7-bit <talha7.webdev@gmail.com>
2025-09-26 20:37:58 -0700

    app

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,19 @@ import Profiledoctor from './admin/Profiledoctor'
 import { ToastContainer,toast } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
 import About from './pages/About'
+import { useEffect } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(()=>{
+    try {
+      const req=useAdmin()
+      console.log("it is working",req)
+    } catch (error) {
+      console.log("an error occured while calling useadmin",error)
+    }
+  })
 
   const {data:authdoctor,authdoctorisLoading}=useAuth()
   const {data:authuser}=useAuthenticate()
